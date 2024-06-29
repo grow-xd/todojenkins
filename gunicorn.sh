72ba5cddc3f17e88edc9c36bae4c2565683bbfd7
grow-xd <gupta.rg.rishabh@gmail.com>
2024-06-29 19:29:38 +0530

    gunicorn setup

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
 source env/bin/activate
+
+cd /var/lib/jenkins/workspace/django-cicd/todocicd
+
+
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+echp "Migration done"
+
+cd /var/lib/jenkins/workspace/django-cicd
+
+sudo cp -rf gunicorn.socket /etc/systemd/system/
+sudo cp -rf gunicorn.service /etc/systemd/system/
+
+echo "$USER"
+echo "$PWD"
+
+sudo systemctl daemon-reload
+sudo systemctl start gunicorn
+sudo systemctl enable gunicorn
+
+echo "Gunicorn started"
+
+sudo systemctl status gunicorn
+sudo systemctl restart gunicorn
+
