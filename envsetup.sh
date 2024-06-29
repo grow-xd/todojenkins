ad7b8cad62351739b9b76d39575eb6f894d065a8
grow-xd <gupta.rg.rishabh@gmail.com>
2024-06-29 18:21:49 +0530

    envsetup changed

--- envsetup.sh
+++ envsetup.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
 
-if [-d "env"]
+if [ -d "env" ] 
 then
-    echo "env virtual env exists"
+    echo "Python virtual environment exists." 
 else
-    echo "env virtual env does not exist"
     python3 -m venv env
-    source env/bin/activate
-    pip install -r requirements.txt
 fi
 
-echo $PWD
 source env/bin/activate
-pip install -r requirements.txt
 
 
-if [ -d "logs" ]
+pip3 install -r requirements.txt
+
+if [ -d "logs" ] 
 then
-    echo "logs directory exists"
+    echo "Log folder exists." 
 else
-    echo "logs directory does not exist"
     mkdir logs
     touch logs/error.log logs/access.log
 fi
 
 sudo chmod -R 777 logs
-echo "envsetup executed"
-
-
