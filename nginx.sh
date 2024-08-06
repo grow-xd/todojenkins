a1ebf0e74411e49e97c11f7b14b4d433342f7c19
grow-xd <gupta.rg.rishabh@gmail.com>
2024-08-06 22:46:41 +0530

    nginx bash changes

--- nginx.sh
+++ nginx.sh
@@ -2,7 +2,7 @@
 
 sudo cp -rf app.conf /etc/nginx/sites-available/todocicd
 chmod 710 /var/lib/jenkins/workspace/django-cicd
-sudo ln -s /etc/nginx/sites-available/todocicd /etc/nginx/sites-enabled
+sudo ln -sf /etc/nginx/sites-available/todocicd /etc/nginx/sites-enabled
 sudo nginx -t
 
 
