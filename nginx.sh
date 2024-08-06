f4bb08df20ea4934c87a6e32feb12486b7a9ec09
grow-xd <gupta.rg.rishabh@gmail.com>
2024-08-06 23:33:38 +0530

    nginx config

--- nginx.sh
+++ nginx.sh
@@ -5,8 +5,6 @@ echo "Nginx configuration copied"
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 sudo ln -sf /etc/nginx/sites-available/todocicd /etc/nginx/sites-enabled
 sudo nginx -t
-sudo systemctl restart nginx
-
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
