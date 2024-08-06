9a59f9aeb5f00aac1e6a4278102c84cfd1760a3c
grow-xd <gupta.rg.rishabh@gmail.com>
2024-08-06 22:50:25 +0530

    nginx conf changed

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo cp -rf app.conf /etc/nginx/sites-available/todocicd
+echo "Nginx configuration copied"
 chmod 710 /var/lib/jenkins/workspace/django-cicd
 sudo ln -sf /etc/nginx/sites-available/todocicd /etc/nginx/sites-enabled
 sudo nginx -t
+sudo systemctl restart nginx
 
 
 sudo systemctl start nginx
